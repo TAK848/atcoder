d9ccf6b166dde91af37f4f87ee196dd3f7131ba3
TAK848 <pairs_char0j@icloud.com>
2022-08-09 11:33:15 +0900

    update

---
+++ math-and-algorithm/022/main.cpp
@@ -0,0 +1,66 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+#define _GLIBCXX_DEBUG
+using namespace std;
+using namespace atcoder;
+using mi = int64_t;
+using vmi = vector<mi>;
+using vvmi = vector<vmi>;
+#define __SPEED_UP__                  \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define rep(i, n) for (mi i = 0; i < (mi)(n); i++)
+#define repstep(i, n, step) for (mi i = (mi)(n); i < (mi)(n) + (mi)(step); i++)
+#define rep2(i, m, n) for (mi i = (mi)(m); i < (mi)(n); i++)
+#define repstep2(i, m, n, step) for (mi i = (mi)(m); i < (mi)(n) + (mi)(step); i++)
+#define all(v) v.begin(), v.end()
+#define YesNo(a) ((a) ? "Yes" : "No")
+#define YESNO(a) ((a) ? "YES" : "NO")
+#define yesno(a) ((a) ? "yes" : "no")
+
+// // aよりもbが大きいならばaをbで更新する
+// // (更新されたならばtrueを返す)
+// template <typename T>
+// bool chmax(T& a, const T& b)
+// {
+//     if (a < b) {
+//         a = b; // aをbで更新
+//         return true;
+//     }
+//     return false;
+// }
+// // aよりもbが小さいならばaをbで更新する
+// // (更新されたならばtrueを返す)
+// template <typename T>
+// bool chmin(T& a, const T& b)
+// {
+//     if (a > b) {
+//         a = b; // aをbで更新
+//         return true;
+//     }
+//     return false;
+// }
+
+// #define endl '\n'
+
+int main()
+{
+    __SPEED_UP__
+    mi n;
+    cin >> n;
+    vector<mi> m(100000, 0);
+    rep(i, n)
+    {
+        mi a;
+        cin >> a;
+        m.at(a)++;
+    }
+    mi ans = 0;
+    rep2(i, 1, 50000)
+    {
+        ans += m.at(i) * m.at(100000 - i);
+    }
+    if (m.at(50000) >= 2)
+        ans += m.at(50000) * (m.at(50000) - 1) / 2;
+    cout << ans << "\n";
+}
